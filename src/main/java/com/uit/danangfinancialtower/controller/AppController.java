d7e62095a937846e2d5140916cce41a4e5779114
Mai Xuan Tu <xuant.mai@m.citynow.vn>
2021-12-13 15:02:40 +0700

    - Refactor: Move all mapping to AppController

--- src/main/java/com/uit/danangfinancialtower/controller/AppController.java
+++ src/main/java/com/uit/danangfinancialtower/controller/AppController.java
@@ -10,10 +10,31 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AppController {
 
+    @RequestMapping({"/", "/index"})
+    public ModelAndView indexPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        return mav;
+    }
+
     @RequestMapping("/appointment")
     public ModelAndView appointmentPage() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("appointment");
+        return mav;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView loginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
+
+    @RequestMapping("/map")
+    public ModelAndView map() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("map3d");
         return mav;
     }
 }
